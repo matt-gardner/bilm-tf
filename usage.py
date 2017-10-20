1c33c107c393528e413cb8c484d9bdf7f286ecc3
Matt Gardner <mattg@allenai.org>
2017-10-20 00:37:20 +0000

    Added cove vector support

--- usage.py
+++ usage.py
@@ -5,6 +5,10 @@ import tqdm
 import numpy
 import h5py
 from bilm import Batcher, BidirectionalLanguageModel
+from spliced.cove import CoVe
+from dl.data import LMTaskVocabulary
+from keras.layers import Input
+from keras.models import Model
 
 class Token:
     def __init__(self, text, pos_tag, dep_label, prev_pos, prev_dep):
@@ -16,39 +20,64 @@ class Token:
         self.next_pos = 'NONE'
         self.next_dep = 'NONE'
 
+COVE = True
+vocab_file = 'vocab.txt'
+options_file = 'skip_connections_options.json'
+weight_file = 'fine_tuned_weights.hdf5'
+out_dir = 'data_cove/'
 
 def main():
-    # Location of pretrained LM.  Here we use the test fixtures.
-    vocab_file = 'vocab.txt'
-    options_file = 'skip_connections_options.json'
-    weight_file = 'fine_tuned_weights.hdf5'
 
-    # Create a Batcher to map text to character ids.
-    batcher = Batcher(vocab_file, 50)
+    if COVE:
+        vocab = LMTaskVocabulary(
+            'ptb_unique_tokens_lower.txt', None, None,
+            lm_normalizers=[lambda x: x.lower()],
+            use_char_inputs=False,
+            mask_zero=True,
+            use_token_key=False,
+            add_bos_eos=False
+        )
 
-    with tf.device('/gpu:0'):
-        # Input placeholders to the biLM.
-        context_character_ids = tf.placeholder('int32', shape=(None, None, 50))
+        cove_encoder = CoVe(
+            {'embedding_weight_file': 'ptb_unique_tokens_lower_glove300.hdf5',
+             'cove_weight_file': 'cove_parameters.hdf5'}
+        )
+        token_ids = Input((None, ), dtype='int32')
+        cove_layers = cove_encoder(token_ids, return_layers=True)
+        cove_model = Model(inputs=token_ids, outputs=cove_layers)
+        cove_model.compile(loss='mse', optimizer='sgd')
 
-        # Build the graphs.  It is necessary to specify a reuse
-        # variable scope (with name='') for every placeholder except the first.
-        # All variables names for the bidirectional LMs are prefixed with 'bilm/'.
-        context_model = BidirectionalLanguageModel(
-            options_file, weight_file, context_character_ids)
+        # UGLY HACK!
+        batcher = vocab
+        context_character_ids = None
+        context_embeddings_op = cove_model
+    else:
+        # Create a Batcher to map text to character ids.
+        batcher = Batcher(vocab_file, 50)
 
-        # Get ops to compute the LM embeddings.
-        context_embeddings_op = context_model.get_ops()['lm_embeddings']
+        with tf.device('/gpu:0'):
+            # Input placeholders to the biLM.
+            context_character_ids = tf.placeholder('int32', shape=(None, None, 50))
+
+            # Build the graphs.  It is necessary to specify a reuse
+            # variable scope (with name='') for every placeholder except the first.
+            # All variables names for the bidirectional LMs are prefixed with 'bilm/'.
+            context_model = BidirectionalLanguageModel(
+                options_file, weight_file, context_character_ids)
+
+            # Get ops to compute the LM embeddings.
+            context_embeddings_op = context_model.get_ops()['lm_embeddings']
 
     train_file = 'wsj/original/stan3-4-dependencies/wsj.train.conllx'
     dev_file = 'wsj/original/stan3-4-dependencies/wsj.dev.conllx'
     test_file = 'wsj/original/stan3-4-dependencies/wsj.test.conllx'
 
     print("Converting dev data")
-    convert_file(batcher, context_character_ids, context_embeddings_op, dev_file, 'data_skip_ft/dev/')
+    convert_file(batcher, context_character_ids, context_embeddings_op, dev_file, out_dir + 'dev/')
     print("Converting training data")
-    convert_file(batcher, context_character_ids, context_embeddings_op, train_file, 'data_skip_ft/train/')
+    convert_file(batcher, context_character_ids, context_embeddings_op, train_file, out_dir + 'train/')
     print("Converting test data")
-    convert_file(batcher, context_character_ids, context_embeddings_op, test_file, 'data_skip_ft/test/')
+    convert_file(batcher, context_character_ids, context_embeddings_op, test_file, out_dir + 'test/')
 
 
 def tokens_from_conll_file(filename):
@@ -92,14 +121,24 @@ def convert_file(batcher, context_character_ids, context_embeddings_op, token_fi
 
         for batch_num, sentence_group in tqdm.tqdm(list(enumerate(grouped_sentences))):
             sentence_text = [[token.text for token in sentence] for sentence in sentence_group]
-            # Create batches of data.
-            context_ids = batcher.batch_sentences(sentence_text)
+            if COVE:
+                vocab = batcher
+                cove_model = context_embeddings_op
+                X_token_ids = vocab.batch_sentences_to_X(sentence_text)['tokens']
+                cove_embeddings = cove_model.predict_on_batch(X_token_ids)
+                # Duplicate the first layer, so that we have a uniformly-shaped tensor.
+                cove_embeddings[0] = numpy.concatenate([cove_embeddings[0], cove_embeddings[0]], axis=-1)
+                # This gives us the same shape result as we have with the non-CoVe case.
+                context_embeddings = [numpy.stack(cove_embeddings, axis=1)]
+            else:
+                # Create batches of data.
+                context_ids = batcher.batch_sentences(sentence_text)
 
-            # Compute LM embeddings.
-            context_embeddings = sess.run(
-                [context_embeddings_op],
-                feed_dict={context_character_ids: context_ids}
-            )
+                # Compute LM embeddings.
+                context_embeddings = sess.run(
+                    [context_embeddings_op],
+                    feed_dict={context_character_ids: context_ids}
+                )
 
             token_jsons = []
             token_embeddings = []
@@ -115,7 +154,7 @@ def convert_file(batcher, context_character_ids, context_embeddings_op, token_fi
                             'next_pos': token.next_pos,
                             'next_dep': token.next_dep,
                             }
-                    token_embeddings.append([embedding[j, i, :] for j in range(3)])
+                    token_embeddings.append([embedding[j][i, :] for j in range(3)])
                     token_jsons.append(token_json)
             token_embeddings = numpy.asarray(token_embeddings)
             with h5py.File(out_dir + 'batch_%d_embeddings.h5' % batch_num, 'w') as out:
